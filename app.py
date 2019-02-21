5cacf9055ff5eadb9c1b6fa43bdc1f72619810ae
Your Name <nguyenminhhieu2508@gmail.com>
2019-02-21 23:22:15 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, request, render_template, flash, session
+# import mlab
+
 app = Flask(__name__)
 
-@app.route('')
+@app.route('/dating4banker', methods = ["GEt", "POST"])
 def home():
+  if request.method == "GET":
+    return render_template("login.html")
+  else:
+    return "Hello"
   
 
 if __name__ == '__main__':
