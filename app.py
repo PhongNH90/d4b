5dcb7a084bf3b329de0e2dcf1a9b7c8631bf7065
Your Name <nguyenminhhieu2508@gmail.com>
2019-03-03 15:13:07 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,62 @@
 from flask import Flask, redirect, request, render_template, flash, session
-# import mlab
+from gmail import GMail, Message
+import mlab
+from models.user import User
 
 app = Flask(__name__)
+mlab.connect()
 
-@app.route('/dating4everyone', methods = ["GET", "POST"])
+
+@app.route("/date4everyone/register", methods = ["GET", "POST"])
+def register():
+  if request.method == "GET":
+    return render_template("register.html")
+  else:
+    form = request.form
+    e = form["em"]  
+    p = form["password"]
+    name = form["name"]
+    u = form["username"]
+    g = form["gender"]
+    b = form["birth"]
+    i = form["img"]
+    c = form["city"]
+    h = form["hobby"]
+    phone = form["phone"]
+    des = form["description"]
+    user_object = User.objects()
+    if u in user_object:
+      flash("Tên người dùng đã tồn tại")
+      return render_template("/date4everyone")
+    else:
+      pass
+    u = User(em=e, password=p, name=name, username=u, gender=g, birth=b, img=i, city=c, hobby=h, phone="+84"+phone, description=des)
+    u.save()
+    gmail = GMail('nguyenminhhieu2508@gmail.com','nguyenminhhieu')
+    msg = Message('text', to = e, html = '<a href="http://127.0.0.1:5000/date4everyone">Xác nhận tài khoản</a>')
+    gmail.send(msg)
+    return "Register"
+
+@app.route("/date4everyone", methods = ["GET", "POST"])
 def home():
   if request.method == "GET":
-    return render_template("login.html")
-  else:
-    return "Hello"
-  
+    return render_template("home.html")
+  elif request.method == "POST":
+    form = request.form
+    u = form["username"]
+    p = form["password"]
+    user = User.objects(username=u).first()
+    if user != None:
+      if user.password == p:
+        session["token"] = u
+        return redirect("/date4everyone")
+      else:
+        flash("Invalid Password")
+        return redirect("/date4everyone")
+    else:
+      flash("User not found")
+      return redirect("/date4everyone")
+    
 
 if __name__ == '__main__':
   app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -1,25 +1,20 @@
 from mongoengine import Document, IntField, StringField, ListField, DictField
 
 class User(Document):
-    username = StringField()
+    em = StringField() # email
     password = StringField()
     name = StringField()
-    fr_name = StringField()
+    username = StringField()
+    # fr_name = StringField()
     gender = StringField()
-    birth = DictField()
+    birth = StringField()
     age = IntField()
     img = StringField()
-    address = StringField()
-    hobby = ListField()
+    city = StringField()
+    hobby = StringField()
     like = IntField()
     interest = ListField()
-    em = StringField() # email
     phone = IntField()
     fb = StringField() # facebook
     description =StringField() # tự mô tả ban thân
     stt = StringField() # Trạng thái: chỉ có 1 trạng thái hiển thị trên trang cá nhân
-
-
-
-
-
